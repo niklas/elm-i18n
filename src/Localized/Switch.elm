d3239e361f93209c9227af71f7338755eb74509a
Niklas Hofer <niklas+dev@lanpartei.de>
2020-03-07 21:56:17 +0100

    Format.

--- src/Localized/Switch.elm
+++ src/Localized/Switch.elm
@@ -4,13 +4,14 @@ module Localized.Switch exposing (generate)
 
     Reads in all the Translation.elm files and generates a master switch for
     them. Elements only present in one of them will still be added.
+
 -}
 
 import Dict exposing (Dict)
 import Localized
 import Localized.Parser as Parser
-import Localized.Writer.Module
 import Localized.Writer.Element exposing (tab)
+import Localized.Writer.Module
 
 
 generate : List Localized.LangCode -> List Localized.SourceCode -> List ( Localized.ModuleName, Localized.SourceCode )
@@ -38,6 +39,7 @@ u list have =
         e :: rest ->
             if member e have then
                 u rest have
+
             else
                 u rest (e :: have)
 
@@ -67,7 +69,7 @@ member e list =
                 ( Localized.ElementFormat m1, Localized.ElementFormat m2 ) ->
                     m1.meta.moduleName == m2.meta.moduleName && m1.meta.key == m2.meta.key
     in
-        List.any (sameElement e) list
+    List.any (sameElement e) list
 
 
 indexBy : (Localized.Element -> comparable) -> List Localized.Element -> Dict comparable (List Localized.Element)
@@ -95,17 +97,17 @@ switchSource languages mod =
         ( moduleName, _ ) =
             mod
     in
-        ( moduleName
-        , Localized.Writer.Module.head mod
-            ++ Localized.Writer.Module.importModuleExposingAll ( "Translation", [] )
-            ++ (String.join "" <|
-                    List.map
-                        (Localized.Writer.Module.importModule << Localized.namedModule << Localized.languageModuleName moduleName)
-                        languages
-               )
-            ++ "\n\n"
-            ++ Localized.Writer.Module.elements (elementSource languages) mod
-        )
+    ( moduleName
+    , Localized.Writer.Module.head mod
+        ++ Localized.Writer.Module.importModuleExposingAll ( "Translation", [] )
+        ++ (String.join "" <|
+                List.map
+                    (Localized.Writer.Module.importModule << Localized.namedModule << Localized.languageModuleName moduleName)
+                    languages
+           )
+        ++ "\n\n"
+        ++ Localized.Writer.Module.elements (elementSource languages) mod
+    )
 
 
 elementSource : List Localized.LangCode -> Localized.Element -> Localized.SourceCode
@@ -120,27 +122,27 @@ elementSource languages element =
         placeholders =
             Localized.Writer.Element.placeholders element
     in
-        name
-            ++ " : Language -> "
-            ++ placeholders
-            ++ "\n"
-            ++ name
-            ++ " language =\n"
-            ++ (tab ++ "case language of\n")
-            ++ (String.join "\n" <|
-                    List.map
-                        (\l ->
-                            (tab ++ tab)
-                                ++ l
-                                ++ " -> "
-                                ++ moduleName
-                                ++ "."
-                                ++ l
-                                ++ "."
-                                ++ name
-                        )
-                        languages
-               )
+    name
+        ++ " : Language -> "
+        ++ placeholders
+        ++ "\n"
+        ++ name
+        ++ " language =\n"
+        ++ (tab ++ "case language of\n")
+        ++ (String.join "\n" <|
+                List.map
+                    (\l ->
+                        (tab ++ tab)
+                            ++ l
+                            ++ " -> "
+                            ++ moduleName
+                            ++ "."
+                            ++ l
+                            ++ "."
+                            ++ name
+                    )
+                    languages
+           )
 
 
 mainModule : List Localized.LangCode -> ( Localized.ModuleName, Localized.SourceCode )
@@ -152,12 +154,12 @@ mainModule languages =
         mod =
             ( name, [] )
     in
-        ( name
-        , Localized.Writer.Module.head mod
-            ++ "type Language = "
-            ++ String.join " | " languages
-            ++ "\n"
-        )
+    ( name
+    , Localized.Writer.Module.head mod
+        ++ "type Language = "
+        ++ String.join " | " languages
+        ++ "\n"
+    )
 
 
 removeLocale : List Localized.LangCode -> Localized.Element -> Localized.Element
